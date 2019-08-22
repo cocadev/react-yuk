9a3934413caf5f1d98e7423a98395e99274ea73b
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-08-22 23:38:50 +0800

    Update landing.js

--- src/screens/Home/landing.js
+++ src/screens/Home/landing.js
@@ -30,9 +30,9 @@ export default class Landing extends React.Component {
               <div className='item-slick1 item1-slick1' style={{ backgroundImage: `url('images/back1.jpg')` }}>
                 <div className="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 
-                  <h4 className="caption2-slide1 tit1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
+                  <h6 className="caption2-slide1 tit1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
                     YuKai is ready for building One page Website
-						      </h4>
+						      </h6>
 
                   <span className="caption1-slide1 txt1 t-center animated visible-false m-b-30" data-appear="fadeInDown">
                     Is your crowd-sourced review site
