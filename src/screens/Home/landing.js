b64ac6322ce49901c2bb614056592c78f0f6e3c4
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-08-22 23:35:39 +0800

    Update landing.js

--- src/screens/Home/landing.js
+++ src/screens/Home/landing.js
@@ -31,7 +31,7 @@ export default class Landing extends React.Component {
                 <div className="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 
                   <h4 className="caption2-slide1 tit1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-                    Blackcheck
+                    YuKai is ready for building One page Website
 						      </h4>
 
                   <span className="caption1-slide1 txt1 t-center animated visible-false m-b-30" data-appear="fadeInDown">
