75680941be9872bb3b5f83a7a29a7f3c4e80d3b2
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-09-14 23:09:05 +0800

    Update landing.js

--- src/screens/Home/landing.js
+++ src/screens/Home/landing.js
@@ -31,7 +31,7 @@ export default class Landing extends React.Component {
                 <div className="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
 
                   <h6 className="caption2-slide1 tit1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-                    BLAQCHECK
+                    BLAQCHECK 
 						      </h6>
 
                   <span className="caption1-slide1 txt1 t-center animated visible-false m-b-30" data-appear="fadeInDown">
