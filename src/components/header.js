c6ddb98ada045bc85b442c7d259342ebe9fbd5f3
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-09-20 15:34:14 +0800

    cursor

--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,33 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Modal from 'react-modal';
 
-
 function MenuItem(props) {
     return (
         <li>
-            {props.title === 'login' && <Link to='#' onClick={() => props.onClick()}>{props.title}</Link>}
-            {props.title !== 'login' && <Link to={props.path}>{props.title}</Link>}
+            <Link to={props.path}>{props.title}</Link>
         </li>
     )
 }
+
+function LoginItem(props) {
+    return (
+        <li onClick={() => props.onLogin()}>
+            <Link>{props.title}</Link>
+        </li>
+    )
+}
+
 
 export default class Header extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            check: 0
         };
         this.openModal = this.openModal.bind(this);
+
         this.closeModal = this.closeModal.bind(this);
     }
 
@@ -28,51 +37,12 @@ export default class Header extends React.Component {
     }
 
     closeModal() {
-        this.setState({ modalIsOpen: false });
+        this.setState({ modalIsOpen: false, check: 0 });
     }
 
-    render() {
-        return (
-            <header>
-                <div className="wrap-menu-header gradient1 trans-0-4">
-                    <div className="container h-full">
-                        <div className="wrap_header trans-0-3">
-                            <div className="logo">
-                                <Link to=''>
-                                    <img src="images/icons/logo.png" alt="IMG-LOGO" data-logofixed="images/icons/logo2.png" />
-                                </Link>
-                            </div>
-
-                            <div className="wrap_menu p-l-45 p-l-0-xl">
-                                <nav className="menu">
-                                    <ul className="main_menu">
-                                        <MenuItem title={'explore'} path={'/'}/>
-                                        <MenuItem title={'learn'} path={'/'}/>
-                                        <MenuItem title={'blog'} path={'/'}/>
-                                        <MenuItem title={'add your business'} path={'/add-business'}/>
-                                        <MenuItem title={'login'} onClick={() => this.openModal()} />
-                                        <MenuItem title={'sign up'} path={'/'}/>
-                                    </ul>
-                                </nav>
-                            </div>
-
-                            <div className="social flex-w flex-l-m p-r-20">
-                                <button className="btn-show-sidebar m-l-33 trans-0-4"></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <Modal
-                    isOpen={this.state.modalIsOpen}
-                    onRequestClose={this.closeModal}
-                    style={customStyles}
-                    ariaHideApp={false}
-                    contentLabel="Example Modal"
-                >
-                    <i className="fa fa-close float-right ft24" aria-hidden="true" onClick={this.closeModal}></i>
-
-                    <div className='p-20'>
+    loginView(){
+        return(
+            <div className='p-20'>
                         <p className="ft20">Login</p>
 
                         <div className="m-t-20">
@@ -116,13 +86,110 @@ export default class Header extends React.Component {
                             </div>
                         </div>
 
-                        <div className="m-t-15">
-                            <Link to={'/'} className='c-red ft13'>Register</Link>&nbsp;|&nbsp;
-				            <Link to={'/'} className='c-red ft13'>Lost your password?</Link>
+                        <div className="m-t-15 flex-row">
+                            <div className='c-red ft13'>Register</div>&nbsp;|&nbsp;
+				            <div className='c-red ft13'>Lost your password?</div>
                         </div>
                     </div>
+        )
+    }
+
+    signUpView(){
+        return(
+            <div className='p-20'>
+                        <p className="ft20">Register</p>
+
+                        <div className="m-t-20">
+                            <p className="ft13">Use a social account for faster login or easy registration.</p>
+                        </div>
+
+                        <div className='flex-c'>
+                            <div className='m-r-10'>
+                                <p className="ft13">First Name</p>
+                                <div className="my-input-border">
+                                    <input type="text" className="myInput sizefull p-l-5" />
+                                </div>
+                            </div>
+                            <div className='m-l-10'>
+                                <p className="ft13">Last Name</p>
+                                <div className="my-input-border">
+                                    <input type="text" className="myInput sizefull p-l-5" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <p className="ft13 m-t-15">Email address <span className="required">*</span></p>
+
+                        <div className="my-input-border">
+                            <input type="text" className="myInput sizefull p-l-5" />
+                        </div>
+
+                        <p className="ft13 m-t-15">Password <span className="required">*</span></p>
+
+                        <div className="my-input-border">
+                            <input type="password" className="myInput sizefull p-l-5" />
+                        </div>
+
+                        <div className="flex-m m-t-22 m-l-2">
+                            <button type="submit" className="btn8 flex-c-m trans-0-4">
+                                Register
+						    </button>
+                        </div>
+
+                        <div className="m-t-15">
+                            <div className='c-red ft13 cursor'>Login</div>
+                        </div>
+                    </div>
+        )
+    }
+
+    render() {
+        const { check } = this.state
+        return (
+            <header>
+                <div className="wrap-menu-header gradient1 trans-0-4">
+                    <div className="container-fluid h-full">
+                        <div className="wrap_header trans-0-3">
+                            <div className="logo">
+                                <Link to=''>
+                                    <img src="images/icons/logo.png" alt="IMG-LOGO" data-logofixed="images/icons/logo2.png" />
+                                </Link>
+                            </div>
+
+                            <div className="wrap_menu">
+                                <nav className="menu">
+                                    <ul className="main_menu">
+
+                                        <MenuItem title={'Explore'} path={'/'} />
+                                        <MenuItem title={'Learn'} path={'/'} />
+                                        <MenuItem title={'Blog'} path={'/'} />
+                                        <MenuItem title={'Add Your Business'} path={'/add-business'} />
+
+                                        <LoginItem title={'Login'} onLogin={() => { this.openModal(); this.setState({ check : 1 })}} />
+                                        <LoginItem title={'Sign Up'} onLogin={() => { this.openModal(); this.setState({ check: 2 })}} />
+
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    ariaHideApp={false}
+                    contentLabel="Example Modal"
+                >
+                    <i className="fa fa-close float-right ft24" aria-hidden="true" onClick={this.closeModal}></i>
+
+                    { check === 1 && this.loginView()}
+                    { check === 2 && this.signUpView()}
 
                 </Modal>
+  
+
             </header>
         )
     }
