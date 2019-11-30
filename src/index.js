2060bf037f18f6dffe75705618796031cf62c190
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-11-30 23:52:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
